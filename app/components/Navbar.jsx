cd94073ed013ceafd5ff1b37aac79dde57d99946
Hassan <kkromah12@gmail.com>
2023-08-21 00:20:11 +0300

    new

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,43 +1,63 @@
-"use client";
-import Link from "next/link";
-import React, { useState } from "react";
-import { FaBars } from "react-icons/fa";
-import { AiOutlineClose } from "react-icons/ai";
+'use client';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
 
-import '../styles/Navbar.css'
+import '../styles/Navbar.css';
 export default function Navbar() {
-  const [click, setClick] = useState(false);
+	const [click, setClick] = useState(false);
+	const [navbarVisible, setNavbarVisible] = useState(true);
 
-  const handleClick = () => setClick(!click);
-  return (
-    <nav className="navbar">
-      <div className="nav-container">
-        <div className="nav-logo">Logo</div>
+	const handleScroll = () => {
+		if (window.scrollY > 550) {
+			setNavbarVisible(false);
+		} else {
+			setNavbarVisible(true);
+		}
+	};
 
-        <ul className={click ? "nav-menu active" : "nav-menu"}>
-          <Link href="/" className="nav-item" onClick={handleClick}>
-            Home
-          </Link>
-          <Link href="#" className="nav-item" onClick={handleClick}>
-            Gallery
-          </Link>
-          <Link href="/products" className="nav-item" onClick={handleClick}>
-            Products
-          </Link>
-          <Link href="/about" className="nav-item" onClick={handleClick}>
-            About
-          </Link>
-          <Link href="/contact" className="nav-item" onClick={handleClick}>
-            Contact
-          </Link>
-        </ul>
-        <div
-          className={click ? "nav-icon active" : "nav-icon"}
-          onClick={handleClick}
-        >
-          {click ? <AiOutlineClose/> : <FaBars />}
-        </div>
-      </div>
-    </nav>
-  );
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
+	const handleClick = () => setClick(!click);
+	return (
+		<nav className={navbarVisible ? 'navbar' : 'navbar navWithBg'}>
+			<div className="nav-container">
+				<div className="nav-logo">Logo</div>
+
+				<ul className={click ? 'nav-menu active' : 'nav-menu'}>
+					<Link href="/" className="nav-item" onClick={handleClick}>
+						Home
+					</Link>
+					<Link href="#" className="nav-item" onClick={handleClick}>
+						Gallery
+					</Link>
+					<Link
+						href="/products"
+						className="nav-item"
+						onClick={handleClick}
+					>
+						Products
+					</Link>
+					<Link href="/about" className="nav-item" onClick={handleClick}>
+						About
+					</Link>
+					<Link href="/contact" className="nav-item" onClick={handleClick}>
+						Contact
+					</Link>
+				</ul>
+				<div
+					className={click ? 'nav-icon active' : 'nav-icon'}
+					onClick={handleClick}
+				>
+					{click ? <AiOutlineClose /> : <FaBars />}
+				</div>
+			</div>
+		</nav>
+	);
 }
