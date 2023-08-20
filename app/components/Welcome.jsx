c249de13e00e6217e95a74e6ac51ea13693e52e9
Musa Kamara <kamaramusa56@gmail.com>
2023-08-20 17:00:48 +0300

    gallery is done

---
+++ app/components/Welcome.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Welcome() {
+  return (
+    <div className="text-center p-5">
+      <h1 className='text-2xl font-bold'> Welcome to my gallery</h1>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -2,13 +2,16 @@ import Services from "./components/Services";
 import Gallery from "./components/Gallery";
 import Slider from "./components/Slider";
 import lightbox from "../public/lightbox.json";
+import Welcome from "./components/Welcome";
+import Experts from "./components/Experts";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Slider />
-
       <Services />
+      <Welcome />
       <Gallery items={lightbox} />
+      <Experts/>
     </main>
   );
 }
