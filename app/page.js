11da41893c4cb52afaf744b476bba3beb8c158cd
Musa Kamara <kamaramusa56@gmail.com>
2023-08-20 17:22:13 +0300

    conflict resolved

--- app/page.js
+++ app/page.js
@@ -3,13 +3,14 @@ import Services from "./components/Services";
 import Gallery from "./components/Gallery";
 import Slider from "./components/Slider";
 import lightbox from "../public/lightbox.json";
+import services from "../public/services.json";
 import Welcome from "./components/Welcome";
 import Experts from "./components/Experts";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Slider />
-      <Services />
+      <Services items={services} />
       <Welcome />
       <Gallery items={lightbox} />
       <Experts/>
