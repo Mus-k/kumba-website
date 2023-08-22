66b9c1c9a4bba39668e0942b41797b8d3769474b
hassan <kkromah12@gmail.com>
2023-08-22 11:35:03 +0300

    new

--- app/orgu/page.jsx
+++ app/orgu/page.jsx
@@ -6,19 +6,26 @@ export default function Orgu() {
 		backgroundImage: "url('/orgu/orgubackground.jpg')",
 		backgroundSize: 'cover',
 		backgroundPosition: 'center',
-		backgroundRepeat:"none",
-		zIndex:-99
+		backgroundRepeat: 'none',
+		zIndex: -99,
 		/* Other styles */
 	};
 
 	return (
-		<div style={componentStyle} className="pt-[85px] flex flex-col justify-center items-center">
-			<h1 className='text-2xl text-white'>HERE IS WHERE YOU CAN FIND OUR BEAUTIFUL BRADING STYLES</h1>
-			<div className="flex flex-wrap justify-center items-center">
+		<div
+			style={componentStyle}
+			className="pt-[90px] flex flex-col justify-center items-center"
+		>
+			<div className='my-12'>
+				<h1 className="text-2xl text-white">
+					HERE IS WHERE YOU CAN FIND OUR BEAUTIFUL BRADING STYLES
+				</h1>
+			</div>
+			<div className="flex flex-wrap justify-around items-center">
 				{orgu.map((item) => (
 					<div key={item.id}>
-						<div className="flex flex-col p-8">
-							<div className="text-white">
+						<div className="flex flex-col mb-8">
+							<div className="text-white p-6 text-xl md:text-2xl">
 								<h1>{item.title}</h1>
 							</div>
 							<div>
@@ -27,9 +34,9 @@ export default function Orgu() {
 									width={500}
 									height={500}
 									alt={item.title}
-									className="w-[350px] h-[300px]"
+									className="w-[330px] h-[300px]"
 								/>
-								<p className='text-white'>&#36;{item.price}</p>
+								<p className="text-white">&#36;{item.price}</p>
 								<div className="w-[300px] text-white">
 									<h2>{item.desc}</h2>
 								</div>
