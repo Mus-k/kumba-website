d87735b75cf8f2f74226b82de0683e4da60e19c4
hassan <kkromah12@gmail.com>
2023-08-23 13:37:12 +0300

    new

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,27 +5,19 @@ const Footer = () => {
 		<div className="footer">
 			<div className="top">
 				<div className="item">
-					<h1>Categoriea</h1>
+					<h1>Categories</h1>
 					<span>Women</span>
 					<span>Men</span>
-					<span>Shoes</span>
 					<span>Accessories</span>
 					<span>New Arrivals</span>
-				</div>
-				<div className="item">
-					<h1>Links</h1>
 					<span>FAQ</span>
-					<span>Pages</span>
-					<span>Stores</span>
-					<span>Compare</span>
-					<span>Cookies</span>
 				</div>
 				<div className="item">
 					<h1>About</h1>
 					<span>
 						Lorem ipsum dolor sit amet consectetur adipisicing elit.
 						Harum, eius explicabo ad magnam quia voluptatem soluta neque
-						illum? Animi, necessitatibus?
+						illum? Animi, necessitatibus.
 					</span>
 				</div>
 				<div className="item">
@@ -33,20 +25,16 @@ const Footer = () => {
 					<span>
 						Lorem ipsum dolor sit amet consectetur adipisicing elit.
 						Harum, eius explicabo ad magnam quia voluptatem soluta neque
-						illum? Animi, necessitatibus?
-						<span>&copy;</span>
+						illum? Animi, necessitatibus.
 					</span>
 				</div>
 			</div>
 			<div className="bottom">
 				<div className="left">
-					<span className="logo">KUMBA SALON</span>
+					<span className="logo">ALYMCIABRAIDING MASTERS</span>
 					<span className="copyright">
 						&copy; Copyright 2023. All Right Revserved
 					</span>
-				</div>
-				<div className="right">
-					<img src="/img/payment.png" alt="" />
 				</div>
 			</div>
 		</div>
