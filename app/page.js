6508e4fb54fbd6f11b3e9d65727f12e4a7bd40e0
Musa Kamara <kamaramusa56@gmail.com>
2023-08-26 23:26:21 +0300

    expert component is done and some other components added

--- app/page.js
+++ app/page.js
@@ -6,14 +6,18 @@ import services from "../public/services.json";
 import Welcome from "./components/Welcome";
 import Experts from "./components/Experts";
 import Details from "./components/Details";
+import SalonIntro from "./components/SalonIntro";
+import CountUpComponent from "./components/CountUp";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Slider />
       <Services items={services} />
-      <Details/>
+      <Details />
       <Welcome />
       <Gallery items={lightbox} />
+      <SalonIntro />
+      <CountUpComponent />
       <Experts />
     </main>
   );
