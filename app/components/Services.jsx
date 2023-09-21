d273a808cec736a70e14afe00c90844f981862a7
Hassan <kkromah12@gmail.com>
2023-09-21 20:31:57 +0300

    images updated

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -24,16 +24,16 @@ export default function Services({ items }) {
 	// };
 
 	return (
-		<div className="flex flex-col w-full bg-pink-100 items-center pb-5 justify-center px-5 border-2 border-purple-500">
+		<div className="flex flex-col w-full bg-pink-100 items-center pb-5 justify-center px-5">
 			<h1 className="text-3xl font-bold p-8 flex items-center">
 				OUR SERVICES
 			</h1>
-			<div className="flex w-full flex-wrap border-2 border-black py-5 justify-around items-center">
+			<div className="flex w-full flex-wrap py-5 justify-around items-center">
 				{items.map((item) => (
 					<Link
 						href={item.url}
 						key={item.id}
-						className="flex flex-col justify-center items-center flex-wrap border-2 border-green-600 w-[30%] relative"
+						className="flex flex-col justify-center items-center flex-wrap w-[30%] relative"
 					>
 						<div className="p-4 text-black/[0.9] flex justify-center items-center">
 							<h2 className="text-lg font-medium">{item.title}</h2>
