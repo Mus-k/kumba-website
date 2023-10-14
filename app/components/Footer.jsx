0947da6a7eaa00db259bf03f6d3f5ec9df128de0
Musa Kamara <kamaramusa56@gmail.com>
2023-10-14 16:54:00 +0300

    removed margin on footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="footer bg-black mx-0 mb-0 my-8">
+    <div className="footer bg-black m-0">
       <div className="top grid md:grid-cols-3 min-w-screen-md sm:p-10 md:px-16 md:pt-8">
         <div className="item w-[100%] text-white">
           <h1 className="font-bold text-2xl text-teal-500">Categories</h1>
--- app/page.js
+++ app/page.js
@@ -1,19 +1,20 @@
-import Services from './components/Services';
-import Slider from './components/Slider';
-import services from '../public/services.json';
-import Experts from './components/Experts';
-import Details from './components/Details';
-import SalonIntro from './components/SalonIntro';
-import CountUpComponent from './components/CountUp';
+import Services from "./components/Services";
+import Slider from "./components/Slider";
+import services from "../public/services.json";
+import Experts from "./components/Experts";
+import Details from "./components/Details";
+import SalonIntro from "./components/SalonIntro";
+import CountUpComponent from "./components/CountUp";
+import "../app/globals.css";
 export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between">
-			<Slider />
-			<Services items={services} />
-			<Details />
-			<SalonIntro />
-			<CountUpComponent />
-			<Experts />
-		</main>
-	);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Slider />
+      <Services items={services} />
+      <Details />
+      <SalonIntro />
+      <CountUpComponent />
+      <Experts />
+    </main>
+  );
 }
