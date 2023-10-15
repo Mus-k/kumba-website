ec8914a81562e0fecb27df8075b7811fd9ecc83c
Musa Kamara <kamaramusa56@gmail.com>
2023-10-16 00:09:41 +0300

    adding card to products page

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,7 +1,30 @@
+import "../styles/Products.css";
+
 export default function products() {
-	return (
-		<div className="bg-teal-200 w-full min-h-screen flex justify-center items-center">
-			<h1 className="text-4xl font-bold">Products coming soon...</h1>
-		</div>
-	);
+  return (
+    <div className="bg-teal-200 w-full min-h-screen flex justify-center items-center">
+      <div class="card">
+        <div class="content">
+          <div class="back">
+            <div class="back-content">
+              <strong>Hover Me</strong>
+            </div>
+          </div>
+          <div class="front">
+            <div class="front-content">
+              <small class="badge">Pasta</small>
+              <div class="description">
+                <div class="title">
+                  <p class="title">
+                    <strong>Spaguetti Bolognese</strong>
+                  </p>
+                </div>
+                <p class="card-footer">order now</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
