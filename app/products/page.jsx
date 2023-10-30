d10270b7e21e21251491687b7172279581e441da
Musa Kamara <kamaramusa56@gmail.com>
2023-10-31 01:06:00 +0300

    almost done

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,30 +1,35 @@
 import "../styles/Products.css";
-
+import product from "../../public/products.json";
 export default function products() {
+  console.log("new", product);
   return (
-    <div className="bg-teal-200 p-5 w-full min-h-screen flex justify-center items-center">
-      <div class="card">
-        <div class="content">
-          <div class="back">
-            <div class="back-content">
-              <strong>Hover Me</strong>
-            </div>
-          </div>
-          <div class="front">
-            <div class="front-content">
-              <small class="badge">00.00TL</small>
-              <div class="description">
-                <div class="title">
-                  <p class="title">
-                    <strong>Spaguetti Bolognese</strong>
-                  </p>
-                  <p class="card-footer">order now</p>
+    <div className="product bg-teal-200">
+      <h1>My products</h1>
+      <div className="w-[100%] flex flex-row flex-wrap gap-3 items-center justify-center">
+        {product.map((productImg) => (
+          <div key={productImg.id} class="card">
+            <div class="content">
+              <div class="back">
+                <div class="back-content">
+                  <strong>{productImg.title}</strong>
                 </div>
-               
+              </div>
+              <div class="front">
+                <div class="front-content">
+                  <small class="badge">{productImg.price}</small>
+                  <div class="description">
+                    <div class="title">
+                      <p class="title">
+                        <strong>{productImg.desc}</strong>
+                      </p>
+                      <p class="card-footer">order now</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
